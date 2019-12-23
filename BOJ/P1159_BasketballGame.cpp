c6230de03840b06620b7bdd4a2eb9ebc4f64c6b7
Jeon, Yeo Hun <21500630@handong.edu>
2019-12-23 20:56:48 +0900

    add comments

--- BOJ/P1159_BasketballGame.cpp
+++ BOJ/P1159_BasketballGame.cpp
@@ -24,8 +24,8 @@ int main (){
         }
         else{
             for (int i = 0 ; i < names.size() ; i++){
-                isNew = true;
-                if (temp.compare(names[i])==0) {
+                isNew = true; // 새로운 애가 등장하는지 확인하는 플래그
+                if (temp.compare(names[i])==0) { // 중복되는 글자가 나오면 횟수 카운터 올리기
                     cnt[i]++;
                     isNew = false;
                     break;
@@ -33,23 +33,23 @@ int main (){
             }
         }
         if (isNew){
-            names.push_back(temp);
+            names.push_back(temp); // 새로 등장하는 애들이 나오면 벡터에 새로 추가해준다.
             cnt.push_back(1);
         }
     }
     bool flag = false;
     vector <string> list;
-    for (int i = 0 ; i < cnt.size() ; i++){
+    for (int i = 0 ; i < cnt.size() ; i++){ // 횟수 카운터가 5가 넘는 애들을 새로운 벡터로 옮기기
         if (cnt[i] >= 5) {
             list.push_back(names[i]);
             flag = true;
         }
     }
-    if (flag == false){
+    if (flag == false){ // 5 이상인 글자들이 없으면 항복선언
         cout << "PREDAJA" << endl;
     }
     else{
-        sort(list.begin(), list.end());
+        sort(list.begin(), list.end()); // 알파벳 순으로 정렬해서 출력해야되니까 정렬하고 출력
         for (string s : list) cout << s;
     }
     return 0;
