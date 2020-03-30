d227c5aed3d19ddf4f297d9416af042593460634
Jeon, Yeo Hun <21500630@handong.edu>
2020-03-30 20:14:04 +0900

    Update P17212_PriceCalcforRabbit.cpp

--- BOJ/P17212_PriceCalcforRabbit.cpp
+++ BOJ/P17212_PriceCalcforRabbit.cpp
@@ -1,22 +1,37 @@
-#include <iostream>
-#include <vector>
+#include <cstdio>
+#include <algorithm>
 
 using namespace std;
 
-int main (){
+int dp[100002];
+
+int main()
+{
     int N;
-    cin >> N;
-    
-    vector<int> coins = {7, 5, 2, 1};
+    scanf("%d", &N);
 
-    int total = 0;
-
-    for (auto coin : coins){
-        total += N/coin;
-        N %= coin;
+    dp[1] = 1;
+    for (int i = 2; i <= N; i++)
+    {
+        if (i % 7 == 0)
+            dp[i] = i / 7;
+        if (i == 2 || i == 5 || i == 7)
+            dp[i] = 1;
+        else
+        {
+            dp[i] = min(dp[i - 1] + 1, dp[i - 2] + 1);
+            if (i >= 5)
+                dp[i] = min(dp[i], dp[i - 5] + 1);
+            if (i >= 7)
+                dp[i] = min(dp[i], dp[i - 7] + 1);
+        }
     }
-
-    cout << total;
-
+    /*
+    for (int i = 1; i <= N; i++)
+    {
+        printf("%d : %d \n", i, dp[i]);
+    }
+*/
+    printf("%d", dp[N]);
     return 0;
 }
