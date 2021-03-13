b5e877522c579f3f494c94fd7c0d0c5ef3a1d564
Jeon, Yeo Hun <21500630@handong.edu>
2021-03-13 21:22:00 +0900

    20210313 Solved

--- BOJ/P14503_RobotCleaner.cpp
+++ BOJ/P14503_RobotCleaner.cpp
@@ -3,171 +3,67 @@
 
 using namespace std;
 
-int map [51][51];
-int result = 1;
 int N, M;
+int map[51][51];
+int steps = 1;
 
-void clean(pair<int, int> position){
-    if(map [position.first][position.second] == 0){
-        result += 1;
-        map[position.first][position.second] = result;
+bool isInMap(int row, int col)
+{
+    return row >= 1 && row <= N && col >= 1 && col <= M;
+}
+
+void dfs(int row, int col, int dir)
+{
+
+    int rowDir[] = {-1, 0, 1, 0};
+    int colDir[] = {0, 1, 0, -1};
+    int rowRearDir[] = {1, 0, -1, 0};
+    int colRearDir[] = {0, -1, 0, 1};
+
+    if (map[row][col] == 0)
+    {
+        steps += 1;
+        map[row][col] = steps;
+    }
+
+    for (int i = dir - 1; i > dir - 5; i--)
+    {
+        int nextDir = (i + 4) % 4;
+        int nextRow = row + rowDir[nextDir];
+        int nextCol = col + colDir[nextDir];
+
+        if (isInMap(nextRow, nextCol) && map[nextRow][nextCol] == 0)
+        {
+            dfs(nextRow, nextCol, nextDir);
+            return;
+        }
+    }
+
+    int nextRearRow = row + rowRearDir[dir];
+    int nextRearCol = col + colRearDir[dir];
+    if (!(map[nextRearRow][nextRearCol] == 1))
+    {
+        dfs(nextRearRow, nextRearCol, dir);
     }
 }
 
-pair<int, int> move(pair<int, int> position, int dir){
-    switch(dir) {
-        case 0 :
-            position = {position.first-1, position.second};
-            break;
-        case 1 :
-            position = {position.first, position.second-1};
-            break;
-        case 2 :
-            position = {position.first+1, position.second};
-            break;
-        case 3 :
-            position = {position.first, position.second+1};
-            break;
-    }
+int main()
+{
 
-    return position;
-}
-
-pair<int, int> rear(pair<int, int> position, int dir){
-    switch(dir) {
-        case 0 :
-            position = {position.first+1, position.second};
-            break;
-        case 1 :
-            position = {position.first, position.second+1};
-            break;
-        case 2 :
-            position = {position.first-1, position.second};
-            break;
-        case 3 :
-            position = {position.first, position.second-1};
-            break;
-    }
-
-    return position;
-}
-
-bool isCleaned(pair<int, int> position, int dir){
-    switch(dir) {
-        case 0 :
-            position = {position.first-1, position.second};
-            break;
-        case 1 :
-            position = {position.first, position.second-1};
-            break;
-        case 2 :
-            position = {position.first+1, position.second};
-            break;
-        case 3 :
-            position = {position.first, position.second+1};
-            break;
-    }
-
-    return map[position.first][position.second] > 1;
-}
-
-bool isWall(pair<int, int> position, int dir){
-    switch(dir) {
-        case 0 :
-            position = {position.first-1, position.second};
-            break;
-        case 1 :
-            position = {position.first, position.second-1};
-            break;
-        case 2 :
-            position = {position.first+1, position.second};
-            break;
-        case 3 :
-            position = {position.first, position.second+1};
-            break;
-    }
-
-    return map[position.first][position.second] == 1;
-}
-
-bool isBound(pair<int, int> position, int dir){
-    switch(dir) {
-        case 0 :
-            position = {position.first-1, position.second};
-            break;
-        case 1 :
-            position = {position.first, position.second-1};
-            break;
-        case 2 :
-            position = {position.first+1, position.second};
-            break;
-        case 3 :
-            position = {position.first, position.second+1};
-            break;
-    }
-
-    return position.first < 1 || position.second < 1 || position.first > N || position.second > M ;
-}
-
-int turnLeft (int dir){
-    dir += 1;
-    dir > 3 ? dir = 0 : dir;
-    
-    return dir;
-}
-
-int turnOpposite (int dir){
-    if (dir < 2) dir += 2;
-    else dir -= 2;
-    
-    return dir;
-}
-
-int main (){
     scanf("%d %d", &N, &M);
 
     int r, c, dir;
     scanf("%d %d %d", &r, &c, &dir);
 
-    for (int i = 0 ; i < N ; i++){
-        for (int j = 0 ; j < M ; j++){
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
             scanf("%d", &map[i][j]);
         }
     }
-    
-    pair<int, int> position = {r, c};
 
-    clean(position);
-    while(true){
-        bool flag = false;
-        for (int i = 0 ; i < 4 ; i ++){
-            dir = turnLeft(dir);
-            if(!isCleaned(position, dir) && !isWall(position, dir) && !isBound(position, dir)){
-                position = move(position, dir);
-                clean(position);
-                flag = true;
-                break;
-            }
-        }
-        
-        if (!flag){
-            dir = turnOpposite(dir);
-            if(isWall(position, dir) || isBound(position, dir)){
-                break;
-            }
-            dir = turnOpposite(dir);
-            position = rear(position, dir);
-        }
-        /*
-        for (int i = 0 ; i < N ; i++){
-            for (int j = 0 ; j < M ; j++){
-                printf("%d\t", map[i][j]);
-            }
-            printf("\n");
-        }
-        printf("\n");
-         */
-    }
+    dfs(r, c, dir);
 
-    printf("%d", result-1);
+    printf("%d", steps - 1);
 }
